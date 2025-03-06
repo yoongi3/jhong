f04d3bda7b4dc827b4f42590d20f49c11ac5a67c
Josh <lolhongy3@gmail.com>
2025-03-06 21:27:53 +1100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jhong/', // Replace 'repositoryname' with your GitHub repository name
   plugins: [react()],
 })
