9ad534809c190d46a798034570580997caf8c1f6
Josh <lolhongy3@gmail.com>
2025-03-13 23:23:00 +1100

    button styles

--- src/Reusable/Button/ButtonStyles.tsx
+++ src/Reusable/Button/ButtonStyles.tsx
@@ -4,9 +4,23 @@ export const PanelButton = styled.button`
     width: 100%;
     height: 20%;
     background-color: #4B0082;
-    color: white;
-    text-shadow: 1px 1px 2px #000000;
+    border: 0.15vw solid #FFFFFF;
+    border-radius: 10px;
+    color: #FFFFFF;
     font-size: 2vw;
+    cursor: pointer;
+
+    &:hover {
+        color: #FF00FF;
+        background-color: #6A0DAD; 
+        transform: scale(1.05); 
+        box-shadow: 0 0 10px 2px #FFD700; 
+    }
+
+    &:active {
+    background-color: #3D0063; 
+    transform: scale(0.95); 
+    }
 `;
 
 export const CloseButton = styled.button`
