513285edbb115b2ad50ec0073ab3c4b0b229f4f8
Josh <lolhongy3@gmail.com>
2025-05-06 04:49:28 +1000

    af2

--- src/ContentOverlay/Content/MyProjects/Projects.tsx
+++ src/ContentOverlay/Content/MyProjects/Projects.tsx
@@ -2,40 +2,40 @@ export const projects = [
     {
         id: 1,
         title: 'TimePick',
-        image: '/Assets/TimePick.png',
+        image: 'Assets/TimePick.png',
         description: 'Team up and sync up! This web app makes group scheduling a breeze — coordinate availabilities, dodge conflicts, and lock in plans with ease. No confusion, just smooth moves!',
         link: 'https://github.com/yoongi3/TimePick'
     },
     {
         id: 2,
         title: 'Volley Bot',
-        image: '/Assets/VolleyBot.png',
+        image: 'Assets/VolleyBot.png',
         description: 'Automate the hunt! This Discord-powered tool retrieves and shares real-time volleyball court availability at Sydney Olympic Park. No more manual checks — just plug in and play!',
         link: 'https://github.com/yoongi3/VolleyBot'
     },
     {
         id: 3,
         title: 'Timesheet Formatter',
-        image: '/Assets/Timesheet.png',
+        image: 'Assets/Timesheet.png',
         description: 'Take the grind out of timesheets! Generate, format, and submit in record time — all precision, no repetition.',
         link: 'https://www.youtube.com/watch?v=p5uHDWBDkCU&ab_channel=JoshuaHong'
     },
     {
         id: 4,
         title: 'Coming Soon...',
-        image: '/Assets/ComingSoon.png',
+        image: 'Assets/ComingSoon.png',
         description: 'coming soon...',
     },
     {
         id: 5,
         title: 'Coming Soon...',
-        image: '/Assets/ComingSoon.png',
+        image: 'Assets/ComingSoon.png',
         description: 'coming soon...',
     },
     {
         id: 6,
         title: 'Coming Soon...',
-        image: '/Assets/ComingSoon.png',
+        image: 'Assets/ComingSoon.png',
         description: 'coming soon...',
     },
     // Add more projects...
